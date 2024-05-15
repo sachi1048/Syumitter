95dfa7e960b6e88601f0733dc8228d36f373d59c
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2024-05-15 12:45:08 +0900

    ログイン画面のHTML
    試し書き

---
+++ USER/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン画面</title>
+</head>
+<body>
+    <h1>Syumitter</h1>
+    <div>
+        <p>login</p>
+        <form action="#" method="POST">
+            <input type="text" name="user" value="ユーザー名">
+            <input type="text" name="pass" value="パスワード">
+            <button type="submit">ログイン</button>
+        </form>
+    </div>
+
+    <a href="#">新規作成</a>
+    <a href="#">パスワードを忘れた</a>
+
+</body>
+</html>
