2e8d99b9b2683fb19eb5823893e068c169397c2e
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2024-05-16 11:05:21 +0900

    css 95%
    ほぼできてます。

--- USER/login.php
+++ USER/login.php
@@ -3,21 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/main.css">
     <title>ログイン画面</title>
 </head>
 <body>
     <h1>Syumitter</h1>
-    <div>
-        <p>login</p>
+    <div class="frame">
+        <p style="font-weight: bold;">login</p>
         <form action="#" method="POST">
-            <input type="text" name="user" value="ユーザー名">
-            <input type="text" name="pass" value="パスワード">
-            <button type="submit">ログイン</button>
+            <input class="textbox" type="text" name="user"  placeholder="ユーザー名"><br>
+            <input class="textbox" type="text" name="pass"  placeholder="パスワード"><br>
+            <button class="nextbotton" type="submit">ログイン</button>
         </form>
     </div>
 
-    <a href="#">新規作成</a>
-    <a href="#">パスワードを忘れた</a>
+    <p><a href="#">新規作成</a></p>
+    <p><a href="#">パスワードを忘れた</p>
 
 </body>
 </html>
