c4b2a1d292cd6486f0d70e2e9f78d817792669ef
fireohiro <148302631+fireohiro@users.noreply.github.com>
2024-05-30 11:28:20 +0900

    投稿画面単体テストのみ終了+CSS

--- USER/toukou.php
+++ USER/toukou.php
@@ -1,5 +1,30 @@
-<?php require 'db-connect.php';
+<?php
+require 'db-connect.php';
 session_start();
+$pdo = new PDO($connect,USER,PASS);
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST['toukousuru'])){
+        $currentDateTime = date('Y-m-d H:i:s');
+        if(isset($_POST['tag1'],$_POST['tag2'],$_POST['tag3'])){
+            $ads=$pdo->prepare('insert into Toukou values(null,?,?,?,?,?,?,?,?)');
+            $ads->execute([$_POST['title'],$currentDateTime,$_POST['naiyou'],$_POST['setumei'],$_POST['tag1'],$_POST['tag2'],$_POST['tag3'],$_SESSION['user']['user_name']]);
+            header("Location: myprofile.php");
+            exit;
+        }else if(isset($_POST['tag1'],$_POST['tag2'])){
+            $ads=$pdo->prepare('insert into Toukou values(null,?,?,?,?,?,?,null,?');
+            $ads->execute([$_POST['title'],$currentDateTime,$_POST['naiyou'],$_POST['setumei'],$_POST['tag1'],$_POST['tag2'],$_SESSION['user']['user_name']]);
+            header("Location: myprofile.php");
+            exit;
+        }else if(isset($_POST['tag1'])){
+            $ads=$pdo->prepare('insert into Toukou values(null,?,?,?,?,?,null,null,?');
+            $ads->execute([$_POST['title'],$currentDateTime,$_POST['naiyou'],$_POST['setumei'],$_POST['tag1'],$_SESSION['user']['user_name']]);
+            header("Location: myprofile.php");
+            exit;
+        }else{
+            echo '<h2>趣味タグを選択してください</h2>';
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -14,26 +39,24 @@ session_start();
     <h1 class="syumitter1">Syumitter</h1> <!-- 上記のロゴ（？） -->
     <form action="toukou.php" method="post"><!-- 投稿ボタンを押したら一回自分の画面を再ロードしてDBにデータを追加してからマイプロフィール画面に飛ばす -->
         <img src="#" alt="未作成"> <!-- 未作成　ここに写真や動画を選択、表示 -->
+        <input type="hidden" name="naiyou" value="toukou1.jpg">
         <p class="koumoku">タイトル</p>
         <input type="text" name="title" required><!-- 投稿テーブルのタイトルに入れる用の入力フォームrequiredを付けることで入力必須項目にしている-->
         <br>
         <?php
-        $pdo = new PDO($connect,USER,PASS);
         // 初期化
         $selectedTags = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (isset($_POST['selectedOptions']) && is_array($_POST['selectedOptions'])) {
+                $count=0;
                 foreach($_POST['selectedOptions'] as $pow){
                     $sel=$pdo->prepare('select * from Tag where tag_id = ?');
                     $sel->execute([$pow]);
-                    $count = 0;
                     foreach($sel as $woe){
                         $count++;
                         echo '<p class="tag_ln">#',$woe['tag_mei'],'</p>';
-                        if($count == 3){
-                            break;
-                        }
+                        echo '<input type="hidden" name="tag',$count,'" value="',$woe['tag_id'];
                     }
                 }
                 // 選択された趣味タグIDを変数に保存した状態
@@ -42,11 +65,11 @@ session_start();
         // ここで$selectedTags変数に選択されたタグのIDが保存されています。
         // $selectedTagsを使って必要な処理を続けることができます。
         ?>
-        <button onclick="location.href='tag_sentaku'">＃趣味タグ追加</button><!-- これから作成予定　この後から画面遷移した後に選択したものをSESSIONに入れる-->
+        <button class="tagbutton" onclick="location.href='tag_sentaku'">＃趣味タグ追加</button><!-- これから作成予定　この後から画面遷移した後に選択したものをSESSIONに入れる-->
         <p class="koumoku">キャプション</p>
         <input type="text" name="setumei" required><!-- 投稿の説明？-->
         <br>
-        <button class="nextbotton" type="submit">投稿</button>
+        <button class="nextbotton" type="submit" name="toukousuru">投稿</button>
     </form>
 </body>
 </html>
