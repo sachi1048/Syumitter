3cf560a74079779a10a7c12351154c3f3806ec7d
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2024-06-24 10:44:37 +0900

    マイプロフィール画面関係　すべて完成

--- USER/myprofile-edit.php
+++ USER/myprofile-edit.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/main.css">
     <script>
-        $(function() {
+        $(document).ready(function() {
             $('.file__label input[type=file]').on('change', function () {
                 var file = $(this).prop('files')[0];
                 $('.file__none').text(file.name);
@@ -39,51 +39,53 @@
         $aikon = $_SESSION['user']['aikon'];
         $profile = $_SESSION['user']['profile'];
 
-        $sql=$pdo->prepare('select * from Account where user_mei=?');
+        $sql=$pdo->prepare('select * from Account where user_name=? ');
         $sql->execute([$user_name]);
+        $row = $sql->fetch(PDO::FETCH_ASSOC);
+        if($row){
     ?>
     <h1 class="h1-1">Syumitter</h1>
 
     <div class="frame">
         <h2>プロフィール編集</h2>
-        <form action="mypofile" method="POST">
+        <form id="passwordForm" action="update_profile.php" method="POST" enctype="multipart/form-data">
         <div class="aikon">
             <lable label="file_label">
                 <img src="img/aikon/<?php echo $aikon; ?>" class="maru">
                 <input type="file" name="aikon">
             </lable>
-            <p class="file_none">変更なし</p>
         </div>
         <table style="margin:auto;">
             <tr>
                 <td>ユーザー名</td>
-                <td><input class="textbox" type="textbox" name="user" placeholder="<?php echo $sql['user_name']; ?>"></td>
+                <td><input type="textbox" name="user" value="<?php echo htmlspecialchars($row['user_name']); ?>"></td>
             </tr>
             <tr>
                 <td>名前</td>
-                <td><input class="textbox" type="textbox" name="display" placeholder="<?php echo $sql['display_name']; ?>"></td>
+                <td><input type="textbox" name="display" value="<?php echo htmlspecialchars($row['display_name']); ?>"></td>
             </tr>
             <tr>
                 <td>プロフィール</td>
-                <td><textarea class="textbox" type="textbox" name="profile" placeholder="<?php echo $sql['profile']; ?>"></td>
+                <td><textarea class="textbox" type="textbox" name="profile"><?php echo htmlspecialchars($row['profile']); ?></textarea></td>
             </tr>
             <tr>
                 <td>アドレス</td>
-                <td><input class="textbox" type="textbox" name="mail" placeholder="<?php echo $sql['mail']; ?>"></td>
+                <td><input type="textbox" name="mail" value="<?php echo htmlspecialchars($row['mail']); ?>"></td>
             </tr>
             <tr>
                 <td>パスワード</td>
-                <td><input class="textbox" type="password" id="password1" name="pass1" placeholder="<?php echo $sql['pass']; ?>" required></td>
+                <td><input type="password" id="password1" name="pass1" value="<?php echo htmlspecialchars($row['pass']); ?>" required></td>
             </tr>
             <tr>
                 <td>確認パスワード</td>
-                <td><input class="textbox" type="password" id="password2" name="pass2" required></td>
+                <td><input type="password" id="password2" name="pass2" required></td>
             </tr>
         </table>
         <p id="errorMessage" class="error">パスワードが一致しません。</p>
         <button class="nextbutton" type="submit">編集</button>
         </form>
     </div>
+    <?php } ?>
     <br>
     <a href="myprofile.php" class="btn-mdr">
         <span class="dli-caret-left"></span>
